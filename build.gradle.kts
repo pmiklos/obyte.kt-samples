90759ef9fc1211d0e6c75a0358688029dca4b97e
Peter Miklos <pmiklos@sirokko.net>
2020-08-02 12:12:26 -0700

    attempt to publish to GitHub pages

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,28 @@ allprojects {
         jcenter()
         mavenCentral()
         mavenLocal()
+        maven {
+            name = "ObyteKt"
+            url = uri("https://maven.pkg.github.com/pmiklos/obytekt")
+            credentials {
+                username = project.findProperty("gpr.user")?.toString() ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key")?.toString() ?: System.getenv("TOKEN")
+            }
+        }
     }
 
+}
+
+tasks.register("gh-pages", Copy::class) {
+    dependsOn("assemble")
+
+    mkdir("$buildDir/gh-pages")
+    destinationDir = file("$buildDir/gh-pages")
+
+    into("simple") {
+        from(project("js:simple").file("build/distributions"))
+    }
+    into("wallet") {
+        from(project("js:wallet").file("build/distributions"))
+    }
 }
